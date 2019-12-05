fdb71c41712a6726817b78d0eb72f4cd98928116
andrbanga <andrbanga@gmail.com>
2019-12-05 13:05:41 +0300

    add move flag

--- scripts/jetbot_joystick.py
+++ scripts/jetbot_joystick.py
@@ -5,15 +5,23 @@ from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
 
 vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+
+enable_mv_flag = False
+
 def joy_teleop(msg):
     k_speed = 0.5
     if msg.buttons[7] == 1:
+        enable_mv_flag = True
+    if msg.buttons[7] == 1:
         k_speed = 1
-    cmd_vel = Twist()
-    cmd_vel.angular.x = msg.axes[0] * k_speed
-    cmd_vel.linear.x = msg.axes[3] * k_speed
 
-    vel_pub.publish(cmd_vel)
+    if enable_mv_flag:
+        cmd_vel = Twist()
+        cmd_vel.angular.x = msg.axes[0] * k_speed
+        cmd_vel.linear.x = msg.axes[3] * k_speed
+        vel_pub.publish(cmd_vel)
+    else
+        
 
 # initialization
 if __name__ == '__main__':
