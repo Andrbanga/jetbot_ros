2d977e124ffe2aa549ca1118603b80fe78f48e75
andrbanga <andrbanga@gmail.com>
2019-12-06 14:38:15 +0300

    add comment and fix readme

--- scripts/jetbot_bringup.py
+++ scripts/jetbot_bringup.py
@@ -7,14 +7,13 @@ from std_msgs.msg import String
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
 
-
-
-
-# sets motor speed between [-1.0, 1.0]
+# set motor speed 
 def set_speed(motor_ID, value):
+	# init max pwm to Adafruit motor driver
 	max_pwm = 115.0
+	# convert velocity val to pwm speed
 	speed = int(min(max(abs(value * max_pwm), 0), max_pwm))
-
+	# choose motor
 	if motor_ID == 2:
 		motor = motor_left
 	elif motor_ID == 1:
@@ -22,9 +21,9 @@ def set_speed(motor_ID, value):
 	else:
 		rospy.logerror('set_speed(%d, %f) -> invalid motor_ID=%d', motor_ID, value, motor_ID)
 		return
-	
+	# set motor speed
 	motor.setSpeed(speed)
-
+	# choose motor direction
 	if value < 0:
 		motor.run(Adafruit_MotorHAT.FORWARD)
 	else:
@@ -39,6 +38,7 @@ def all_stop():
 	motor_left.run(Adafruit_MotorHAT.RELEASE)
 	motor_right.run(Adafruit_MotorHAT.RELEASE)
 
+# simple kinematic to convert robot cmd_vel to each motor velocity
 def on_cmd_vel(cmd_vel):
     left_motor_vel = cmd_vel.linear.x + cmd_vel.angular.x
     right_motor_vel = cmd_vel.linear.x - cmd_vel.angular.x
@@ -52,10 +52,10 @@ if __name__ == '__main__':
 
 	# setup motor controller
 	motor_driver = Adafruit_MotorHAT(i2c_bus=1)
-
+	# init motor id
 	motor_left_ID = 1
 	motor_right_ID = 2
-
+	# init motors
 	motor_left = motor_driver.getMotor(motor_left_ID)
 	motor_right = motor_driver.getMotor(motor_right_ID)
 
@@ -64,7 +64,7 @@ if __name__ == '__main__':
 
 	# setup ros node
 	rospy.init_node('jetbot_joy_teleop')
-	
+	# init subscriber
 	rospy.Subscriber('cmd_vel', Twist, on_cmd_vel)
 
 
--- scripts/jetbot_joystick.py
+++ scripts/jetbot_joystick.py
@@ -4,30 +4,45 @@ import time
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
 
+# init ros publisher to publish velocity 
 vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
-
+# init enable moving flag
 enable_mv_flag = False
 
+# func that convert joy msg to velocity
 def joy_teleop(msg):
+    # coeff to control speed
     k_speed = 0.5
-    if msg.buttons[7] == 1:
+    # if press "X" enable moving
+    if msg.buttons[0] == 1:
         enable_mv_flag = True
+    # if press "O" disable moving    
+    if msg.buttons[1] == 1:
+        enable_mv_flag = False
+    # while "R1" button is pressed enable speed boost
     if msg.buttons[7] == 1:
         k_speed = 1
-
+    # if enable mv flag true, convert joy to vel and publish it
     if enable_mv_flag:
+        # init cmd_vel msg use Twist type which contain angular and linear vel
         cmd_vel = Twist()
+        # convert left stick value to angular vel
         cmd_vel.angular.x = msg.axes[0] * k_speed
+        # convert right stick value to linear vel
         cmd_vel.linear.x = msg.axes[3] * k_speed
+        # publish cmd_vel 
         vel_pub.publish(cmd_vel)
-    else
-        
+    # else publish 0 vel to cmd_vel
+    else:
+        cmd_vel = Twist()
+        vel_pub.publish(cmd_vel)
 
 # initialization
 if __name__ == '__main__':
 
 	# setup ros node
 	rospy.init_node('jetbot_joy_teleop')
+    # init subscriber to recieve joy msg
 	rospy.Subscriber('joy', Joy, joy_teleop)
     
 	# start running
